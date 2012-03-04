e39674be7f700d3e342be57bb987f9919d093b7f
Brad Lucas <brad@beaconhill.com>
2012-03-04 08:46:31 -0500

    add to clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject quote-downloader "1.0.0"
+(defproject org.clojars.blucas/quote-downloader "1.0.0"
   :description "quote-downloader"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :main quote-downloader.core)
