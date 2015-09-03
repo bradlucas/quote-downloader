974977e92f41f518d7841289c336016efaa4501a
Brad Lucas <brad@beaconhill.com>
2015-09-03 14:30:14 -0400

    updated version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject org.clojars.blucas/quote-downloader "1.0.0"
   :description "quote-downloader"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :main quote-downloader.core)
