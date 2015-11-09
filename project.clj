aed2ad8ab06f5faecb9f3a3ddf6b513ec6bb7aea
Brad Lucas <brad@beaconhill.com>
2015-11-09 13:34:47 -0500

    integrating command line options

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject org.clojars.blucas/quote-downloader "1.0.0"
   :description "quote-downloader"
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.cli "0.3.3"]]
   :main quote-downloader.core)
--- src/quote_downloader/core.clj
+++ src/quote_downloader/core.clj
@@ -1,5 +1,8 @@
 (ns quote-downloader.core
-  (:require [clojure.java.io :as io])
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]
+            [clojure.tools.cli :refer [cli]]
+            [clojure.java.io :as io])
   (:gen-class :main true))
 
 (defn build-url [sym]
@@ -31,13 +34,47 @@
   (doall(pmap download-historical-quotes lst))
   (shutdown-agents))
 
-(defn print-usage []
-  (println "quote-downloader SYMBOL"))
+(defn print-usage [options-summary]
+  (println (->> [""
+                 "Usage: quote-downloader [options] [symbols]"
+                 ""
+                 "Options:"
+                 options-summary
+                 ""
+                 "Symbols:"
+                 "Optional list of symbols to download"
+                 "Use -f option as an alternative to load a larger number of symbols"
+                 ""]
+                (clojure.string/join \newline))))
+
+(defn load-symbol-file 
+  "Read a file of symbols and return them in a sequence"
+  [filename]
+  (with-open [rdr (io/reader filename)]
+    (doall (line-seq rdr))))
 
 (defn -main 
-  "Accept a list of stock symbols as arguments otherwise print a usage statement"
-  [& args]  
-  (if args (time (process-symbols-parallel args))
-      (print-usage)))
-
-
+  [& args]
+  (let [[opts args summary]
+        ;; https://github.com/bradlucas/cmdline/blob/master/src/cmdline/core.clj
+        (cli args
+             ["-l" "--log" "Enable log messages" :flag true :default true]
+             ["-p" "--parallel" "Parrallel loading" :flag true :default true]
+             ["-f" "--file-symbols" "File containing symbols to load (overloads symbols passed as args)"]
+             )]
+    ;; if no file-symbols optionand no args show usage because there is nothing to do
+    (if (and (not (:file-symbols opts)) (not (seq args)))
+      (print-usage summary)
+      (do
+        (let [log (:log opts)
+              parallel (:parallel opts)
+              symbols (if (:file-symbols opts) (load-symbol-file (:file-symbols opts)) args)]
+          (println "log:" log)
+          (println "parallel:" opts)
+          (println "symbols:" symbols)
+          (time (process-symbols-parallel symbols))
+          )
+        )
+      )
+    )
+  )
