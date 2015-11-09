bd4340c3c02ba081c61580f981fddeafbcb0c931
Brad Lucas <brad@beaconhill.com>
2015-11-08 21:24:59 -0500

    use pmap to run downloads in parallel

--- src/quote_downloader/core.clj
+++ src/quote_downloader/core.clj
@@ -23,7 +23,16 @@
   (doseq [sym lst]
    (download-historical-quotes sym)))
 
+(defn process-symbols-p [lst]
+  ;; Call shutdown-agents adfter pmap otherwise the program will take a long time to shutdown
+  ;; http://stackoverflow.com/questions/8695808/clojure-program-not-exiting-when-finishing-last-statement
+  ;; http://stackoverflow.com/questions/2622750/why-does-clojure-hang-after-having-performed-my-calculations/2622788#2622788
+  (doall(pmap download-historical-quotes lst))
+  (shutdown-agents))
+
 (defn -main [& args]
   ;; accept a list of stock symbols as arguments otherwise print a usage statement
-  (if args (process-symbols args)
+  (if args (time (process-symbols-p args))
       (print-usage)))
+
+
