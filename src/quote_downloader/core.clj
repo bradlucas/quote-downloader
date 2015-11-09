4b5e374724447e17ad353a0c82113125ddb545a3
Brad Lucas <brad@beaconhill.com>
2015-11-09 12:01:44 -0500

    comments

--- src/quote_downloader/core.clj
+++ src/quote_downloader/core.clj
@@ -16,23 +16,28 @@
         (println (format "%s" sym)))
       (catch Exception e (println (format "Error downloading '%s'" sym))))))
 
-(defn print-usage []
-  (println "quote-downloader SYMBOL"))
-
-(defn process-symbols [lst]
+(defn process-symbols-sequential 
+  "Process a list of symbols sequentially"
+  [lst]
   (doseq [sym lst]
    (download-historical-quotes sym)))
 
-(defn process-symbols-p [lst]
-  ;; Call shutdown-agents adfter pmap otherwise the program will take a long time to shutdown
-  ;; http://stackoverflow.com/questions/8695808/clojure-program-not-exiting-when-finishing-last-statement
-  ;; http://stackoverflow.com/questions/2622750/why-does-clojure-hang-after-having-performed-my-calculations/2622788#2622788
+(defn process-symbols-parallel 
+  "Process a list of symbols in parallel
+  Call shutdown-agents after pmap otherwise the program will take a long time to shutdown
+  @see http://stackoverflow.com/questions/8695808/clojure-program-not-exiting-when-finishing-last-statement
+  @see http://stackoverflow.com/questions/2622750/why-does-clojure-hang-after-having-performed-my-calculations/2622788#2622788"
+  [lst]
   (doall(pmap download-historical-quotes lst))
   (shutdown-agents))
 
-(defn -main [& args]
-  ;; accept a list of stock symbols as arguments otherwise print a usage statement
-  (if args (time (process-symbols-p args))
+(defn print-usage []
+  (println "quote-downloader SYMBOL"))
+
+(defn -main 
+  "Accept a list of stock symbols as arguments otherwise print a usage statement"
+  [& args]  
+  (if args (time (process-symbols-parallel args))
       (print-usage)))
 
 
