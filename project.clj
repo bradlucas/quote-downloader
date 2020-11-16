2675ffb6e68ba9cc1a9ec0b484f0ebeee6bc904a
Brad Lucas <brad@beaconhill.com>
2020-11-15 22:40:44 -0500

    new group com.bradlucas

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.blucas/quote-downloader "1.0.1"
+(defproject com.bradlucas/quote-downloader "1.0.1"
   :description "quote-downloader"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.5"]]
